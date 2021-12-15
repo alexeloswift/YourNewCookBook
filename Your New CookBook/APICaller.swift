0e87d5380a34cb7c6b50ac0f6525643bbe7f1e32
Alex Diaz <alexelo.swift@gmail.com>
2021-12-15 11:19:59 -0500

    fixed error message

--- Your New CookBook/APICaller.swift
+++ Your New CookBook/APICaller.swift
@@ -9,11 +9,25 @@ import Foundation
 import UIKit
 
 
-enum ErrorMessages: String, Error {
-    case invalidURL         = "Invalid URL"
-    case unableToComplete   = "You should check your connection"
-    case invalidResponse    = "Invalid response from server"
-    case invalidData        = "The data recieved from server is invalid"
+enum ErrorMessages: Error {
+    case invalidURL
+    case unableToComplete
+    case invalidResponse
+    case invalidData
+}
+extension ErrorMessages: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .unableToComplete:
+            return "You should check your connection"
+        case .invalidResponse:
+            return "Invalid response from server"
+        case .invalidData:
+            return "The data recieved from server is invalid"
+        }
+    }
 }
 
 class APICaller {
