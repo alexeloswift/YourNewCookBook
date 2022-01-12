89e6544311963097ce588e17d7473b91fffe1ed8
Alex Diaz <alexelo.swift@gmail.com>
2022-01-12 17:22:00 -0500

    fixed imports

--- Your New CookBook/Managers/NetworkManager.swift
+++ Your New CookBook/Managers/NetworkManager.swift
@@ -5,7 +5,6 @@
 //  Created by Alexis Diaz on 12/12/21.
 //
 
-import Foundation
 import UIKit
 
 
--- Your New CookBook/ViewControllers/MealViewController.swift
+++ Your New CookBook/ViewControllers/MealViewController.swift
@@ -5,9 +5,6 @@
 //  Created by Alexis Diaz on 12/12/21.
 //
 
-import Foundation
-import UIKit
-
 
 import UIKit
 
